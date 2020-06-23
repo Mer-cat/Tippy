4399620303d7e7ded1a313757ba04ce96047c0d0
Mercy Bickell <mercycat@fb.com>
2020-06-23 14:22:43 -0600

    Initial Commit

---
+++ Tippy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tippy
+//
+//  Created by Mercy Bickell on 6/23/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Tippy/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Tippy
+//
+//  Created by Mercy Bickell on 6/23/20.
+//  Copyright © 2020 mercycat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
